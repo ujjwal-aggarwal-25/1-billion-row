0bf82a3727ebfc46e55c3944879daf0d74f89c3f
aggarwal-ujjwal <aggarwal.ujjwal.17@gmail.com>
2026-09-21 23:52:45 +0530

    MAIN: Add argument parser argument

--- createData.py
+++ createData.py
@@ -6,7 +6,6 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Create measurement file")
     parser.add_argument("-o","--output", dest="ouput", type=str, help="Measurement file name - default is measurements.txt",default="measurements.txt")
-
-
+    parser.add_argument("-r", "--records", dest="records", type=min_records, help="Number of records to create - default is 1_000_000_000", default=1_000_000_000)
 
 
